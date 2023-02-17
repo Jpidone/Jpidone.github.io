317cb3d7b1d4120517e2d7f0c597ad4690f35391
Javier <jpidone@gmail.com>
2023-02-17 14:59:54 -0300

    Agregada Amnimación y Mejorado de margenes

---
+++ main.js
@@ -0,0 +1,16 @@
+const typed = new Typed('.typed', {
+    strings: [
+    'Tec. en Electrónica', 
+    'Tec. Operación y Mantenimiento de Redes Eléctricas', 
+    'Desarrolador Junior'],
+
+typeSpeed: 40,
+startDelay: 300,
+backSpeed: 5,
+shuffle: false,
+backDelay: 1500,
+loop: true,
+loopCount: false,
+showCursor: true,
+coursoChar: "/",
+});
